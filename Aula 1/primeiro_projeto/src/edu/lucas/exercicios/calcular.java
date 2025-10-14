514067f0aba0adc7ce88e8e38dfd774b345d7dd2
LucasRoberto07 <lucasrobertocaetanodeassis@gmail.com>
2025-10-14 16:54:02 -0300

    começando novo modulo de estudos

---
+++ Aula 1/primeiro_projeto/src/edu/lucas/exercicios/calcular.java
@@ -0,0 +1,26 @@
+package edu.lucas.exercicios;
+
+
+
+public class calcular {
+
+    public int somar(int a, int b) {
+        return a + b;
+    }
+
+    public int subtrair(int a, int b) {
+        return a - b;
+    }
+
+    public int multiplicar(int a, int b) {
+        return a * b;
+    }
+
+    public int dividir(int a, int b) {
+        if (b != 0) {
+            return a / b;
+        } else {
+            throw new IllegalArgumentException("Divisão por zero não é permitida.");
+        }
+    }
+}
